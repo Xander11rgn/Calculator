3f8b8b4a1bde891a16b765c18d9db8f04feab1cd
Xander11rgn <xander11rgn-cool@yandex.ru>
2021-11-09 17:59:19 +0300

    fix

--- test/CalculatorLibrary.Tests/CalculatorTests.cs
+++ test/CalculatorLibrary.Tests/CalculatorTests.cs
@@ -110,5 +110,93 @@ namespace CalculatorLibrary.Tests
 
             Assert.Equal(expectedOutput, Regex.Replace(output.ToString(), @"[\r\t\n]+", string.Empty));
         }
+
+        [Fact]
+        public void ShouldBeAbleToRunProgramMainWithWrongFirstParam()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            var output = new StringWriter();
+            Console.SetOut(output);
+            var input = new StringReader("awfg" + Environment.NewLine + "156.15" + Environment.NewLine + "-6548.0012" + Environment.NewLine + "d" + Environment.NewLine + "n");
+            Console.SetIn(input);
+
+            Program.Main(new string[] { });
+
+            var expectedOutput = "Console Calculator in C#" +
+                                 "------------------------" +
+                                 "Type a number, and then press Enter: " +
+                                 "This is not valid input. Please enter an integer value: " +
+                                 "Type another number, and then press Enter: " +
+                                 "Choose an operator from the following list:" +
+                                 "a - Add" +
+                                 "s - Subtract" +
+                                 "m - Multiply" +
+                                 "d - Divide" +
+                                 "Your option? " +
+                                 "Your result: -0.02" +
+                                 "------------------------" +
+                                 "Press 'n' and Enter to close the app, or press any other key and Enter to continue: ";
+
+            Assert.Equal(expectedOutput, Regex.Replace(output.ToString(), @"[\r\t\n]+", string.Empty));
+        }
+
+        [Fact]
+        public void ShouldBeAbleToRunProgramMainWithWrongSecondParam()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            var output = new StringWriter();
+            Console.SetOut(output);
+            var input = new StringReader("156.15" + Environment.NewLine + "egsge" + Environment.NewLine + "-6548.0012" + Environment.NewLine + "d" + Environment.NewLine + "n");
+            Console.SetIn(input);
+
+            Program.Main(new string[] { });
+
+            var expectedOutput = "Console Calculator in C#" +
+                                 "------------------------" +
+                                 "Type a number, and then press Enter: " +
+                                 "Type another number, and then press Enter: " +
+                                 "This is not valid input. Please enter an integer value: " +
+                                 "Choose an operator from the following list:" +
+                                 "a - Add" +
+                                 "s - Subtract" +
+                                 "m - Multiply" +
+                                 "d - Divide" +
+                                 "Your option? " +
+                                 "Your result: -0.02" +
+                                 "------------------------" +
+                                 "Press 'n' and Enter to close the app, or press any other key and Enter to continue: ";
+
+            Assert.Equal(expectedOutput, Regex.Replace(output.ToString(), @"[\r\t\n]+", string.Empty));
+        }
+
+
+        [Fact]
+        public void ShouldBeAbleToRunProgramMainDBZ()
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en-US");
+            var output = new StringWriter();
+            Console.SetOut(output);
+            var input = new StringReader("156.15" + Environment.NewLine + "0" + Environment.NewLine + "d" + Environment.NewLine + "n");
+            Console.SetIn(input);
+
+            Program.Main(new string[] { });
+
+            var expectedOutput = "Console Calculator in C#" +
+                                 "------------------------" +
+                                 "Type a number, and then press Enter: " +
+                                 "Type another number, and then press Enter: " +
+                                 "Choose an operator from the following list:" +
+                                 "a - Add" +
+                                 "s - Subtract" +
+                                 "m - Multiply" +
+                                 "d - Divide" +
+                                 "Your option? " + 
+                                 "Oh no! An exception occurred trying to do the math. - Details: " +
+                                 "Token PropertyName in state Property would result in an invalid JSON object. Path 'Operations[0]'." +
+                                 "------------------------" +
+                                 "Press 'n' and Enter to close the app, or press any other key and Enter to continue: ";
+
+            Assert.Equal(expectedOutput, Regex.Replace(output.ToString(), @"[\r\t\n]+", string.Empty));
+        }
     }
 }
